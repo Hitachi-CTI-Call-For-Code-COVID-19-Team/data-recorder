52af31a9f078ea5364de48823eb1dc337a421d3e
Watanabe Kentaro <kentaro.watanabe.dc@hitachi.com>
2020-07-21 09:41:02 +0900

    support the multiple messages on a es trigger

--- cloud-functions/process-message.js
+++ cloud-functions/process-message.js
@@ -1,20 +1,25 @@
-function main(params) {
-        var result = params.messages[0].value
-        console.log(params.messages)
-        result["format"] = "json"
-        var dt = new Date()
-        result.timestamp = dt.toISOString()
-        return {
-                    dbname: "iotp_o9ypqz_default_" + getDateString(dt),
-                    doc: result
-                }
+ function main(params) {
+    var dt
+    var docs = params.messages.map(message => {
+        doc = message.value
+        doc["format"] = "json"
+        dt = new Date()
+        doc.timestamp = dt.toISOString()
+        return doc
+    })
+    //console.log(docs)
+    console.log("iotp_o9ypqz_default_" + getDateString(dt))
+    return {
+        dbname: "iotp_o9ypqz_default_" + getDateString(dt),
+        docs: { docs: docs}
+    }
 }
 
 function getDateString(dt) {
-        var YYYY = dt.getUTCFullYear()
-        var month = dt.getUTCMonth() + 1
-        var MM = month < 10 ? '0' + month : month
-        var day = dt.getUTCDate()
-        var DD = day < 10 ? '0' + day : day
-        return YYYY + "-" + MM + "-" + DD
+    var YYYY = dt.getUTCFullYear()
+    var month = dt.getUTCMonth() + 1
+    var MM = month < 10 ? '0' + month : month
+    var day = dt.getUTCDate()
+    var DD = day < 10 ? '0' + day : day
+    return YYYY + "-" + MM + "-" + DD
 }

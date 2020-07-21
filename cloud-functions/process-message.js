f6fa0483741d2f8cd4a5bed9625bf835449c76b0
Watanabe Kentaro <kentaro.watanabe.dc@hitachi.com>
2020-07-21 09:37:24 +0900

    only record first doc based on es trigger

---
+++ cloud-functions/process-message.js
@@ -0,0 +1,20 @@
+i function main(params) {
+        var result = params.messages[0].value
+        console.log(params.messages)
+        result["format"] = "json"
+        var dt = new Date()
+        result.timestamp = dt.toISOString()
+        return {
+                    dbname: "iotp_o9ypqz_default_" + getDateString(dt),
+                    doc: result
+                }
+}
+
+function getDateString(dt) {
+        var YYYY = dt.getUTCFullYear()
+        var month = dt.getUTCMonth() + 1
+        var MM = month < 10 ? '0' + month : month
+        var day = dt.getUTCDate()
+        var DD = day < 10 ? '0' + day : day
+        return YYYY + "-" + MM + "-" + DD
+}
